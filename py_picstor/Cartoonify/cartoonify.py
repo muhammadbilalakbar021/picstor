9bf6f455738dc5eef92fe0fee023f70b7c2e9299
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2021-06-30 09:15:05 -0700

    Update cartoonify.py

--- py_picstor/Cartoonify/cartoonify.py
+++ py_picstor/Cartoonify/cartoonify.py
@@ -3,44 +3,47 @@ import numpy as np
 
 from rootPath import get_root_path
 
+
 class Cartoonify:
-    def __init__(self, i_Image):
-        self.img = cv2.imread(i_Image)
-        self.line_size = None
-        self.blur_value = None
-        self.total_color = None
-        self.output = get_root_path()+'/Images/Output/'
+  def __init__(self, i_Image):
+    self.img = cv2.imread(i_Image)
+    self.line_size = None
+    self.blur_value = None
+    self.total_color = None
+    self.output = get_root_path() + '/Images/Output/'
 
-    def color_quantization(self):
-        # Transform the image
-        data = np.float32(self.img).reshape((-1, 3))
+  def color_quantization(self):
+    # Transform the image
+    data = np.float32(self.img).reshape((-1, 3))
 
-        # Determine criteria
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
+    # Determine criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
 
-        # Implementing K-Means
-        ret, label, center = cv2.kmeans(data, self.total_color, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-        center = np.uint8(center)
-        result = center[label.flatten()]
-        result = result.reshape(self.img.shape)
-        return result
+    # Implementing K-Means
+    ret, label, center = cv2.kmeans(data, self.total_color, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    result = center[label.flatten()]
+    result = result.reshape(self.img.shape)
+    return result
 
-    def edge_mask(self):
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        gray_blur = cv2.medianBlur(gray, self.blur_value)
-        edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, self.line_size, self.blur_value)
-        return edges
+  def edge_mask(self):
+    gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.medianBlur(gray, self.blur_value)
+    edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, self.line_size,
+                                  self.blur_value)
+    return edges
 
-    def cartoon_Image(self):
-        self.line_size = 7
-        self.blur_value = 7
-        self.total_color = 9
-        edges = self.edge_mask()
-        img = self.color_quantization()
-        blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
-        cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
-        cv2.imwrite(self.output + 'CartoonResult.jpg', cartoon)
+  def cartoon_Image(self):
+    self.line_size = 7
+    self.blur_value = 7
+    self.total_color = 9
+    edges = self.edge_mask()
+    img = self.color_quantization()
+    blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200, sigmaSpace=200)
+    cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
+    cv2.imwrite(self.output + 'CartoonResult.jpg', cartoon)
+
 
 if __name__ == "__main__":
-    test = Cartoonify(get_root_path()+'/Images/Input/test3.jpeg')
-    test.cartoon_Image()
+  test = Cartoonify(get_root_path() + '/Images/Input/test3.jpeg')
+  test.cartoon_Image()
