274c3d46bcde05d3b704ea9658042501729bb2c6
Abdur Rehman <statelybridge99@gmail.com>
2021-07-06 22:59:44 +0500

    Front end updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,36 +12,36 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
   slides = [
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/48.jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/50.jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/30.jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/29.jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/27.jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/51.jpg"},
-    {img: "https://mdbootstrap.com/img/Photos/Others/images/54.jpg"},
+    { img: "assets/images/p8.jpg" },
+    { img: "assets/images/p2.jpg" },
+    { img: "assets/images/p10.jpg" },
+    { img: "assets/images/p4.jpg" },
+    { img: "assets/images/p5.jpg" },
+    { img: "assets/images/p6.jpg" },
+    { img: "assets/images/p7.jpg" },
   ];
-  slideConfig = {"slidesToShow": 4, "slidesToScroll": 2, "autoplay": true, "autoplaySpeed": 1000, "speed": 3000,};
-  
+  slideConfig = { "slidesToShow": 4, "slidesToScroll": 2, "autoplay": true, "autoplaySpeed": 1000, "speed": 3000, };
+
   addSlide() {
-    this.slides.push({img: "http://placehold.it/350x150/777777"})
+    this.slides.push({ img: "http://placehold.it/350x150/777777" })
   }
-  
+
   removeSlide() {
     this.slides.length = this.slides.length - 1;
   }
-  
+
   slickInit(e: any) {
     console.log('slick initialized');
   }
-  
+
   breakpoint(e: any) {
     console.log('breakpoint');
   }
-  
+
   afterChange(e: any) {
     console.log('afterChange');
   }
-  
+
   beforeChange(e: any) {
     console.log('beforeChange');
   }
